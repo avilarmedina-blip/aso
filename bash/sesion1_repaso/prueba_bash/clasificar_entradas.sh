6693be53d4d8b68445bc7436c73af3944c7469de
Toni Vilar <avilarmedina@gmail.com>
2026-09-22 12:29:30 +0200

    ejerecicio clasificar_entradas corregido

--- bash/sesion1_repaso/prueba_bash/clasificar_entradas.sh
+++ bash/sesion1_repaso/prueba_bash/clasificar_entradas.sh
@@ -1,9 +1,8 @@
 for entrada in ~/aso/bash/sesion1_repaso/prueba_bash/*; do
-    if [[ -f $entrada ]]; then
-        basename $entrada
-        echo "archivo"
-    else
-        basename $entrada
-        echo "directorio"
+    nombre=$(basename "$entrada")
+    if [[ -d $entrada ]]; then
+        echo "$nombre: directorio"
+    elif [[ -f $entrada ]]; then
+        echo "$nombre: fichero"
     fi
 done
